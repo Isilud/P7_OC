e6ecb3c80a33fb4a15b9fd71049ca4a84356bfcf
Isilud <jolan.houot@gmail.com>
2022-10-04 15:38:47 +0200

    Initial commit

---
+++ kasa/src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/App.css'
+
+function App () {
+  return (
+    <>
+      App
+    </>
+  )
+}
+
+export default App
---
+++ kasa/src/pages/FicheLogement.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/FicheLogement.scss'
+
+function FicheLogement () {
+  return (
+    <>
+      FicheLogement
+    </>
+  )
+}
+
+export { FicheLogement }
