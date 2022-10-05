2c2865a034cbf8b7ff4dfe423b349e72d60afbbe
Isilud <jolan.houot@gmail.com>
2022-10-05 11:02:58 +0200

    Added Pages & Router

--- kasa/src/components/App.jsx
+++ kasa/src/components/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import Router from '../router/Router'
+import { BrowserRouter } from 'react-router-dom'
+import { Header, Footer } from './'
+
 import '../styles/App.css'
 
 function App () {
   return (
-    <>
-      App
-    </>
+    <div className="app">
+      <BrowserRouter>
+        <Header />
+        <div className="page_container">
+          <Router />
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
   )
 }
 
---
+++ kasa/src/router/Router.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import { Home, APropos, FicheLogement, NotFound } from '../pages'
+
+function Router () {
+  return (
+    <React.StrictMode>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/APropos" element={<APropos />} />
+          <Route exact path="/FicheLogement" element={<FicheLogement />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+    </React.StrictMode>
+  )
+}
+export default Router
