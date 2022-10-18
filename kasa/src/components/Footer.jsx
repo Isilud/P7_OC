da1ca06edd104afd8acf070078d2f2826d2b05eb
Isilud <jolan.houot@gmail.com>
2022-10-18 14:15:51 +0200

    Added Footer

--- kasa/src/components/Footer.jsx
+++ kasa/src/components/Footer.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+
 import '../styles/Footer.scss'
+import variables from '../styles/utils/colors.scss'
+
+import { Logo } from './Logo'
 
 function Footer () {
-  return <div className="footer">Footer</div>
+  return (
+    <div className="footer">
+      <div className="footer_logo">
+      <Logo color={variables.color_white} />
+      </div>
+      <div className="footer_text">
+        © 2020 Kasa. All rights reserved
+      </div>
+    </div>
+  )
 }
 
 export { Footer }
