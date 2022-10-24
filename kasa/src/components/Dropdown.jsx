51fd341ddee6b7982dcdefc12497e23781e8f12b
Isilud <jolan.houot@gmail.com>
2022-10-24 15:58:29 +0200

    Added dropdown

---
+++ kasa/src/components/Dropdown.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+
+import { ReactComponent as ArrowLogo } from '../assets/Arrow.svg'
+
+import '../styles/Dropdown.scss'
+
+function Dropdown ({ title, children }) {
+  const [isClose, setIsClose] = useState(true)
+
+  const toogle = () => {
+    setIsClose(!isClose)
+  }
+
+  return (
+    <div className="dropdown">
+      <div className="dropdown_label" onClick={() => toogle()}>
+        {title}
+        <ArrowLogo transform={isClose ? 'scale(1,1)' : 'scale(1,-1)'} />
+      </div>
+      <div
+        className={`dropdown_content dropdown_content${
+          isClose ? '-closed' : '-open'
+        }`}
+      >
+        {children}
+      </div>
+    </div>
+  )
+}
+
+Dropdown.propTypes = {
+  title: PropTypes.string,
+  children: PropTypes.any
+}
+
+export { Dropdown }
