e0650a9f477ac865a0659e22ba3d945f8d273e18
Isilud <jolan.houot@gmail.com>
2022-10-31 09:26:03 +0100

    fixup! Project update

--- kasa/src/pages/NotFound.jsx
+++ kasa/src/pages/NotFound.jsx
@@ -1,11 +1,14 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react'
 import '../styles/NotFound.scss'
 
 function NotFound () {
   return (
-    <>
-      Not_Found
-    </>
+    <div className="notfound">
+      <div>404</div>
+      <div>Oups! La page que vous demandez n'existe pas.</div>
+      <div>Retourner sur la page d’accueil</div>
+    </div>
   )
 }
 
